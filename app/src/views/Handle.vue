40c77d7204ce2db628e3f54166366c06533db21c
smilebuz <smile_buz@163.com>
2017-09-08 15:46:48 +0800

    add handle side

--- app/src/views/Handle.vue
+++ app/src/views/Handle.vue
@@ -1,8 +1,47 @@
 <template lang="html">
   <div class="handle">
     <div class="side">
-      <div class="">
-
+      <div class="dbSelect">
+        <Select style="width: 140px;" size="small">
+          <Option v-for="(db, index) in dbs" :key="db.id" :value="db.pdbName">
+            {{ db.pdbName }}
+          </Option>
+        </Select>
+        <Button type="text" shape="circle" icon="refresh"></Button>
+      </div>
+      <Tree :data="tables" class="tree"></Tree>
+      <div class="radios">
+        <div class="radio-div" v-for="(value, key) in radios" :key="key" :style="radioChecked(value.checked)" @click="checkTBInfo(key)">
+          {{ value.label }}
+        </div>
+      </div>
+      <div class="tbInfo" v-show="radios.fields.checked">
+        <div class="tbInfo__title">
+          <div class="tbInfoText">名称</div>
+          <div class="tbInfoText">类型</div>
+        </div>
+        <div v-for="(field, index) in fields" :key="field.fieldName" class="tbInfo__content">
+          <div class="tbInfoText">
+            {{ field.fieldName }}
+          </div>
+          <div class="tbInfoText">
+            {{ field.fieldType }}
+          </div>
+        </div>
+      </div>
+      <div class="tbInfo" v-show="radios.partitions.checked">
+        <div class="tbInfo__title">
+          <div class="tbInfoText">名称</div>
+          <div class="tbInfoText">个数</div>
+        </div>
+        <div v-for="(partition, index) in partitions" :key="partition.partitionName" class="tbInfo__content">
+          <div class="tbInfoText">
+            {{ partition.partitionName }}
+          </div>
+          <div class="tbInfoText">
+            {{ partition.partitionNum }}
+          </div>
+        </div>
       </div>
     </div>
     <div class="sqlpad">
@@ -21,15 +60,91 @@
             <Button @click="showSql(tab.content)">测试brace</Button>
           </TabPane>
         </Tabs>
+        <Tabs type="card">
+          <TabPane label="数据">
+            <Table border stripe :columns="dataColumns" :data="dataList"></Table>
+          </TabPane>
+          <TabPane label="日志">
+            <div class="logpad">
+              <p v-for="(log, index) in logs" class="logpad__log">
+                {{ log.content }}
+              </p>
+            </div>
+          </TabPane>
+        </Tabs>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// import updateCompletions from '../utils/updateCompletions.js'
+
 export default {
   data () {
     return {
+      dbs: [
+        {
+          'pdbId': 1,
+          'pdbName': 'ocr'
+        },
+        {
+          'pdbId': 2,
+          'pdbName': 'ocr'
+        },
+        {
+          'pdbId': 3,
+          'pdbName': 'ocr'
+        }
+      ],
+      tables: [
+        {
+          expand: true,
+          title: 'ocr',
+          children: [
+            {
+              title: 'user_list'
+            },
+            {
+              title: 'user_list'
+            }
+          ]
+        }
+      ],
+      radios: {
+        fields: {
+          label: '字段',
+          checked: true
+        },
+        partitions: {
+          label: '分区',
+          checked: false
+        }
+      },
+      fields: [
+        {
+          fieldName: 'Id',
+          fieldType: 'int'
+        },
+        {
+          fieldName: 'name',
+          fieldType: 'STRING'
+        },
+        {
+          fieldName: 'age',
+          fieldType: 'int'
+        }
+      ],
+      partitions: [
+        {
+          partitionName: 'name',
+          partitionNum: 2
+        },
+        {
+          partitionName: 'age',
+          partitionNum: 3
+        }
+      ],
       operations: [
         {
           name: '运行',
@@ -71,10 +186,78 @@ export default {
       ],
       sqlEditor: {
         rows: 5
-      }
+      },
+      schemaInfo: {
+      },
+      dataColumns: [
+        {
+          type: 'index',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: '客户编码',
+          key: ''
+        },
+        {
+          title: '保单号',
+          key: ''
+        },
+        {
+          title: '用户名称',
+          key: ''
+        },
+        {
+          title: '险种',
+          key: ''
+        },
+        {
+          title: '车牌号',
+          key: ''
+        },
+        {
+          title: '状态',
+          key: ''
+        },
+        {
+          title: '保费',
+          key: ''
+        }
+      ],
+      dataList: [],
+      logs: [
+        {
+          content: '2017.09.09 12:20:20  运行正常'
+        },
+        {
+          content: '2017.09.09 12:20:20  运行正常'
+        },
+        {
+          content: '2017.09.09 12:20:20  运行正常'
+        },
+        {
+          content: '2017.09.09 13:20:20  出现警告信息：SQL语法错误，请重新编辑!'
+        }
+      ]
     }
   },
   methods: {
+    radioChecked (checked) {
+      if (checked) {
+        return {
+          background: '#66b8f0',
+          color: '#fff'
+        }
+      }
+    },
+    checkTBInfo (radioName) {
+      for (let prop in this.radios) {
+        if (this.radios.hasOwnProperty(prop)) {
+          this.radios[prop].checked = false
+        }
+      }
+      this.radios[radioName].checked = true
+    },
     operationStyle (imgUrl) {
       return {
         background: 'url(' + imgUrl + ') no-repeat left center'
@@ -101,6 +284,7 @@ export default {
       })
     }
   }
+
 }
 </script>
 
@@ -111,10 +295,50 @@ export default {
   }
   .side {
     flex-basis: 170px;
-    background-color: #008080;
+    text-align: left;
+  }
+  .dbSelect {
+    display: flex;
+    margin-left: 5px;
+    align-items: center;
+  }
+  .radios {
+    display: flex;
+    margin-top: 50px;
+  }
+  .radio-div {
+    flex: 1;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    cursor: pointer;
+    background: #dcdcdc;
+  }
+  .tree {
+    margin-left: 5px;
+  }
+  .tbInfo__title {
+    background: #f8f8f8;
+  }
+  .tbInfo__title,
+  .tbInfo__content {
+    display: flex;
+  }
+  .tbInfo__content:last-child {
+    border-bottom: 1px solid #f0f0f0;
+  }
+  .tbInfoText {
+    flex: 1;
+    height: 30px;
+    border: 1px solid #f0f0f0;
+    border-bottom: none;
+    border-right: none;
+    line-height: 30px;
+    text-align: center;
   }
   .sqlpad {
     flex-grow: 1;
+    max-width: calc(100% - 170px);
     background: #f0f0f0;
   }
   .opgroup {
@@ -136,5 +360,16 @@ export default {
     height: 150px;
     padding: 1em;
     font-size: 14px;
+  }
+  .logpad {
+    height: 100%;
+    padding: 1em;
+    border: 1px solid #efefef;
+    border-radius: 5px;
+    text-align: left;
+    background: #fff;
+  }
+  .logpad__log {
+    margin-bottom: .5em;
   }
 </style>
